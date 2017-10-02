b838170c1392fb48771ad1caabf439adc085b8a4
IncredibleQuark <lkupinski@gmail.com>
2017-10-02 23:58:46 +0200

    creating method for background select depending on weather

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-
 var openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather';
 if (window.navigator && window.navigator.geolocation) {
     window.navigator.geolocation.getCurrentPosition(function(position) {
@@ -10,8 +9,30 @@ if (window.navigator && window.navigator.geolocation) {
         }).done(function(data) {
             console.log(data);
 
+            selectBackground(data.weather[0].id);
+            document.getElementById('city').innerText = data.name;
 
 
         })
     })
 }
+
+
+function selectBackground(code) {
+
+    var result;
+
+    if (code === 800 || (code < 958 && code >= 951)) {
+        result = "clear";
+    }
+      else if (code >= 500 && code < 600) {
+        result = "rain";
+    }
+
+    console.log(result);
+
+    var background = document.querySelector('body');
+    background.classList.add(result);
+
+}
+
